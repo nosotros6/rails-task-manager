be6086d2b5591816746f6f6bab303637409aeee5
pilar garcia <pilargarciaroyo@gmail.com>
2021-07-21 14:05:38 -0400

    task manager dia 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get 'tasks', to: 'tasks#index' 
+get 'tasks/:id', to: 'tasks#show', as: :task
+get 'tasks/new', to: 'tasks#new', as: :new_task
+post 'tasks', to: 'tasks#create' 
+get 'tasks/:id/edit', to: 'tasks#edit'
+patch 'task/:id', to: 'tasks#update'
+delete 'task/:id', to: 'task#destroy'
 end
